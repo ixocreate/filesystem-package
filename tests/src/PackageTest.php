01306dc872b5ba29b4c917d9b76ac6d0ba728c34
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-04-26 14:59:45 +0200

    add package test

---
+++ tests/src/PackageTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Test\Filesystem;
+
+use Ixocreate\Application\Configurator\ConfiguratorRegistryInterface;
+use Ixocreate\Application\Service\ServiceRegistryInterface;
+use Ixocreate\Filesystem\ConfigProvider;
+use Ixocreate\Filesystem\Package;
+use Ixocreate\ServiceManager\ServiceManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+class PackageTest extends TestCase
+{
+    /**
+     * @covers \Ixocreate\Filesystem\Package
+     */
+    public function testPackage()
+    {
+        $configuratorRegistry = $this->getMockBuilder(ConfiguratorRegistryInterface::class)->getMock();
+        $serviceRegistry = $this->getMockBuilder(ServiceRegistryInterface::class)->getMock();
+        $serviceManager = $this->getMockBuilder(ServiceManagerInterface::class)->getMock();
+
+        $package = new Package();
+        $package->configure($configuratorRegistry);
+        $package->addServices($serviceRegistry);
+        $package->boot($serviceManager);
+
+        $this->assertNull($package->getBootstrapItems());
+        $this->assertNull($package->getConfigDirectory());
+        $this->assertSame([
+            ConfigProvider::class,
+        ], $package->getConfigProvider());
+        $this->assertNull($package->getDependencies());
+        $this->assertDirectoryExists($package->getBootstrapDirectory());
+    }
+}
